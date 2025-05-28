1311da74bde7e5934b3a1fca68b253e9024d4973
narich <narich@yanolja.com>
2025-05-28 17:42:36 +0900

    clean code

--- front_dabang/src/app/layout.js
+++ front_dabang/src/app/layout.js
@@ -2,16 +2,13 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './components/layouts/Header.js'
 import Navigator from './components/layouts/Navigator.js'
-import SignUp from './signup/page'
 import Page from "./page.js";
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '80%', height:'100%', margin: '0 auto' ,backgroundColor:'Grey'}}>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '80%', height:'100%', margin: '0 auto' ,backgroundColor:'lightgrey'}}>
             <div style={{ height:'15%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
               <Header/>
             </div>
