71be8d13797ac3df64eb4b0859a7f0144e10d120
kjo <awsedr27@naver.com>
2025-07-31 09:52:52 +0900

    미들웨어 수정

--- front_dabang/src/middleware.js
+++ front_dabang/src/middleware.js
@@ -2,10 +2,18 @@ import { NextResponse } from 'next/server';
 import { verifyToken, signAccessToken } from '@/lib/jwt';
 
 export function middleware(request) {
+  const { pathname } = request.nextUrl
+  console.log('미들웨어 진입');
+  // ✅ 특정 경로 예외 처리
+  const publicPaths = ['/api/auth']
+  if (publicPaths.some(path => pathname.startsWith(path))) {
+    return NextResponse.next()
+  }
+
   const accessToken = request.cookies.get('accessToken')?.value;
   const refreshToken = request.cookies.get('refreshToken')?.value;
   // 토큰 없으면 쿠키 삭제 후 로그인 페이지로 리다이렉트
-  console.log(accessToken);
+  console.log('미들웨어 통과');
   if (!accessToken) {
     const res = NextResponse.json(
       { error: 'No access token', code: 'NO_ACCESS_TOKEN' ,routeUrl : '/login'},
@@ -66,10 +74,16 @@ export function middleware(request) {
 
   // 토큰이 유효하지 않은 경우
   if (!payload) {
-    const response = NextResponse.redirect(new URL('/login', request.url));
-    response.cookies.delete('accessToken', { path: '/' });
-    response.cookies.delete('refreshToken', { path: '/' });
-    return response;
+    const res = NextResponse.json(
+        { error: 'No refresh token', code: 'NO_REFRESH_TOKEN' ,routeUrl : '/login'},
+        { status: 401 }
+    );
+
+    // 쿠키 삭제
+    res.cookies.set('accessToken', '', { maxAge: 0, path: '/' });
+    res.cookies.set('refreshToken', '', { maxAge: 0, path: '/' });
+
+    return res;
   }
 
   // 정상 토큰일 경우 요청 계속 진행
